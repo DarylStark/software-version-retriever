d313cecb5df80b0e8b0de3680f91f8badf681fc3
Daryl Stark <daryl@dstark.nl>
2022-11-14 15:34:47 +0100

    Updated the main import file for the Software Versions package

--- src/version_checker/__init__.py
+++ src/version_checker/__init__.py
@@ -1,4 +1,10 @@
+# Base class for Version Checkers
 from .version_checker import VersionChecker
+
+# Classes to retrieve from REST APIs
 from .restapi_version_checker import RestAPIVersionChecker
 from .restapi_json_version_checker import RestAPIJSONVersionChecker
+
+# Classes to retrieve from GitHub
+from .github_version_checker import GitHubVersionChecker
 from .github_release_version_checker import GitHubReleaseVersionChecker

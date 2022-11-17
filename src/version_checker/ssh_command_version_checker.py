f948311c0443a91f24284ae9641ae29ed061e82f
Daryl Stark <daryl@dstark.nl>
2022-11-17 09:25:06 +0100

    Added a timeout to SSH connections

--- src/version_checker/ssh_command_version_checker.py
+++ src/version_checker/ssh_command_version_checker.py
@@ -9,12 +9,14 @@ class SSHCommandVersionChecker(ShellCommandVersionChecker):
                  remote_server: str,
                  username: str,
                  identity_file: str = None,
+                 timeout: int = 10,
                  **kwargs) -> None:
         """ Set configuration values """
         super().__init__(**kwargs)
         self.remote_server = remote_server
         self.username = username
         self.identity_file = identity_file
+        self.timeout = timeout
 
     def get_command_output(self) -> str:
         """ Method to run the command on the remote host and retrieve the
@@ -24,7 +26,8 @@ class SSHCommandVersionChecker(ShellCommandVersionChecker):
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
         ssh.connect(self.remote_server,
                     username=self.username,
-                    key_filename=self.identity_file)
+                    key_filename=self.identity_file,
+                    timeout=self.timeout)
         stdin, stdout, stderr = ssh.exec_command(self.command)
         lines = ''.join(stdout.readlines()).strip()
         return lines
